794e2a11d983d18418618084724b57fcbd90294a
GAURAB <gaurabkhadka4082@gmail.com>
2025-02-14 19:05:10 +0545

    Implement landing page with routing and Bootstrap integration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,26 @@
+import React, { Suspense } from "react";
 import "./App.css";
-import LoginComponent from "./Components/LoginComponent";
-import RegisterComponent from "./Components/RegisterComponent";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Landing = React.lazy(() => import("./Components/LandingPage"));
 
 function App() {
   return (
-    <>
-      <LoginComponent />
-      <RegisterComponent />
-    </>
+    <Router>
+      <Suspense fallback={<div>Loading....</div>}>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/main"} />} />
+          <Route path="/main" element={<Landing />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ Frontend/src/Components/LandingPage.jsx
@@ -0,0 +1,37 @@
+import "../Styles/LandingPage.css";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <div className="heroSection">
+        {/* <div className="navigation">
+          <ul>
+            <li>Home</li>
+            <li>Trekking package</li>
+            <li>Agencies</li>
+          </ul>
+
+          <ul>
+            <li>
+              <button>Login</button>
+            </li>
+            <li>
+              <button>Signup</button>
+            </li>
+          </ul>
+        </div> */}
+        {/* Hero section content*/}
+        <div className="hs-content">
+          <div className="titleCard">
+            <h1>THE CLIMB</h1>
+            <p>Discover breathtaking mountain trails</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
+}
