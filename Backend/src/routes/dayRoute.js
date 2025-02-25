e1e1397ab2fd58d2f46b883a454d3182f9eb99b4
GAURAB <gaurabkhadka4082@gmail.com>
2025-02-25 23:56:30 +0545

    Remove day-related controller, model, and routes; clean up unused code

--- Backend/src/routes/dayRoute.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const dayController = require("../controller/dayController");
-// const dayRouter = express.Router();
-
-// dayRouter.post("/", dayController.create);
-// dayRouter.get("/", dayController.getAllday);
-
-// module.exports = dayRouter;
